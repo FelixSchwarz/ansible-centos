3a6acf995053e9e3cc8c6b9d2e5813754483c0f1
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2016-11-09 09:31:44 +0100

    letsencrypt: adapt to certbot 0.9

--- roles/letsencrypt/files/letsencrypt-renew.sh
+++ roles/letsencrypt/files/letsencrypt-renew.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-/usr/bin/certbot renew $* | grep '/fullchain.pem' | grep ' (' | grep -v '(skipped)'
+# certbot 0.9 prints the following to stderr:
+# > Saving debug log to /var/log/letsencrypt/letsencrypt.log
+# > Cert not yet due for renewal
+# redirecting stderr to stdout so we can filter the output
+# (fewer cron messages)
+/usr/bin/certbot renew $* 2>&1 | grep '/fullchain.pem' | grep ' (' | grep -v '(skipped)'
 
