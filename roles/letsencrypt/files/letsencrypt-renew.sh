13663e04379d18494f9c78f612694e3afccdf5dd
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2016-11-14 09:28:42 +0100

    common/certbot: ensure that all errors are sent by mail while retaining a "certificate updated" notification

--- roles/letsencrypt/files/letsencrypt-renew.sh
+++ roles/letsencrypt/files/letsencrypt-renew.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-# certbot 0.9 prints the following to stderr:
-# > Saving debug log to /var/log/letsencrypt/letsencrypt.log
-# > Cert not yet due for renewal
-# redirecting stderr to stdout so we can filter the output
-# (fewer cron messages)
-/usr/bin/certbot renew $* 2>&1 | grep '/fullchain.pem' | grep ' (' | grep -v '(skipped)'
+# --quiet: print only errors
+/usr/bin/certbot renew --quiet $*
+
+# cron "notification" about updated certificates
+find /etc/letsencrypt/live -name 'cert.pem' -type f -follow -mtime -1
 
