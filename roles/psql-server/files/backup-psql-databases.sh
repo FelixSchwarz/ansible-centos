374cd87cb95dff5c9e2e7910f569179ce322bfe5
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2018-09-12 10:20:28 +0200

    psql-server: run psql backup script as "postgres" user

--- roles/psql-server/files/backup-psql-databases.sh
+++ roles/psql-server/files/backup-psql-databases.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 # This file is managed by Ansible. Don't make changes here, they will be overwritten.
 
+# The "root" user may not exist as postgres user. Trying switching to "postgres".
+PSQL_USER=postgres
+USER=$(id --name --user)
+if [ "${USER}" != "${PSQL_USER}" ]; then
+   su - "${PSQL_USER}" --shell=/bin/bash --command ${BASH_SOURCE}
+   RC=$?
+   exit $RC
+fi
+
 DUMPDIR=/srv/borgtmp
 
 DBLIST=`psql --dbname=postgres --quiet --tuples-only --command='SELECT datname from pg_database' | grep -v template`
